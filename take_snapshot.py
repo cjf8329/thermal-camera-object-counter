51fd6ece271d6fcf7a0e353323955bf7fcc18553
Christopher Fonseca <cjf8329@nyu.edu>
2023-06-14 19:14:10 -0400

    added sample frame; i'm gonna try a different approach. i'm looking for clusters of pixels within certain temperature ranges without using opencv, hopefully making this thing kinda foolproof. should also run way faster and with less computation.

--- take_snapshot.py
+++ take_snapshot.py
@@ -13,9 +13,9 @@ print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
 # if using higher refresh rates yields a 'too many retries' exception,
 # try decreasing this value to work with certain pi/camera combinations
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_8_HZ
 
-scalar = 20
+scalar = 20  
 w = scalar * 32
 h = scalar * 24
 getFrame_output = [0] * 768
@@ -32,13 +32,15 @@ while True:
         maxVal = math.floor(np.amax(img))
         minVal = math.ceil(np.amin(img))
 
-        img = img - minVal
-        img = img * 255/(maxVal - minVal)
-        imgGray = img.astype(np.uint8)
-        img = cv2.applyColorMap(imgGray, cv2.COLORMAP_JET)
+        norm_img = img - minVal
+        norm_img = img * 255/(maxVal - minVal)
+        norm_imgGray = norm_img.astype(np.uint8)
+        norm_img = cv2.applyColorMap(norm_imgGray, cv2.COLORMAP_JET)
 
 
-        print("done.")
+        print("done.", end="")
+        print('Average MLX90640 Temperature: {0:2.1f}C ({1:2.1f}F)'.\
+      format(np.mean(frame),(((9.0/5.0)*np.mean(frame))+32.0)))
 
         final = cv2.resize(img, (img.shape[1] * s, img.shape[0] * s))
 
@@ -47,5 +49,7 @@ while True:
     except KeyboardInterrupt:
         print("err")
         break
+
+print(getFrame_output)
 
 cv2.destroyAllWindows()
--- thermal_module.py
+++ thermal_module.py
@@ -3,28 +3,53 @@ import board
 import busio
 import adafruit_mlx90640
 import numpy as np
+import cv2
+import math
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
 # if using higher refresh rates yields a 'too many retries' exception,
 # try decreasing this value to work with certain pi/camera combinations
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
 
+scalar = 20
+w = scalar * 32
+h = scalar * 24
 getFrame_output = [0] * 768
-final_frame = np.zeros((24, 32), dtype=float)
+final_frame = np.zeros([h, w, 3])
 while True:
     try:
         mlx.getFrame(getFrame_output)
-    except ValueError:
-        print("val_error")
-        continue
+        #scaling factor
+        s = 10
 
-    for h in range(24):
-        for w in range(32):
-            pixel = getFrame_output[h*32 + w]
-            final_frame[h][w] = pixel
-        print()
-    print()
+        img = np.array(getFrame_output)
+        img.shape = (24, 32)
+
+        maxVal = math.floor(np.amax(img))
+        minVal = math.ceil(np.amin(img))
+
+        norm_img = img - minVal
+        norm_img = img * 255/(maxVal - minVal)
+        norm_imgGray = norm_img.astype(np.uint8)
+        norm_img = cv2.applyColorMap(norm_imgGray, cv2.COLORMAP_JET)
+
+        print("done.", end="")
+        print('Average MLX90640 Temperature: {0:2.1f}C ({1:2.1f}F)'.\
+      format(np.mean(getFrame_output),(((9.0/5.0)*np.mean(getFrame_output))+32.0)))
+
+        final = cv2.resize(norm_img, (img.shape[1] * s, img.shape[0] * s))
+        #final = cv2.resize(norm_imgGray, (img.shape[1] * s, img.shape[0] * s))
+
+        cv2.imshow("final", final)
+        cv2.waitKey(50)
+    except KeyboardInterrupt:
+        print("err")
+        break
+
+print(getFrame_output)
+
+cv2.destroyAllWindows()
